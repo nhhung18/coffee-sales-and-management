bc93f200636f0441e09003780c6f3cfa44f7bf6f
nhhung18 <nguyenhung18042005@gmail.com>
2025-09-03 10:32:45 +0700

    create prj

---
+++ src/test/java/com/hungng/coffe_restaurant_system/CoffeRestaurantSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hungng.coffe_restaurant_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeRestaurantSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
